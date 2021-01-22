2af4e45096d6fc69d5dff63b5b80b5463c73cd2d
Mike McGrath <mokeseven7@gmail.com>
2021-01-22 07:12:54 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-terraform {
+vterraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
@@ -9,10 +9,10 @@ terraform {
   }
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "mokeseven7"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "mokeseven7"
     }
   }
 }
